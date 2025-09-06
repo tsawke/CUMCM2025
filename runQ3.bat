9b372089db4891d2437ac6d18801beb44a7258cc
tsawke@dorm <tsawke@outlook.com>
2025-09-07 02:49:11 +0800

    update

--- runQ3.bat
+++ runQ3.bat
@@ -113,16 +113,18 @@
 @REM   --fuse_min 3 --fuse_max 6 --fuse_step 1
 
 
-python Q3Solver.py ^
-  --dt 0.005 --nphi 360 --nz 7 ^
-  --outer_backend process ^
-  --backend thread --workers 1 --chunk 2200 --block 4096 ^
-  --heading_min_deg -2 --heading_max_deg 2 --heading_step_deg 0.5 ^
-  --speed_min 120 --speed_max 140 --speed_step 10 ^
-  --t1_min 0 --t1_max 6 --t1_step 1 ^
-  --gap12_min 1 --gap12_max 4 --gap12_step 1 ^
-  --gap23_min 1 --gap23_max 4 --gap23_step 1 ^
-  --fuse_min 3 --fuse_max 8 --fuse_step 1 ^
-  --stage2_enable --stage2_span_steps 1 ^
-  --stage2_heading_step_deg 0.2 --stage2_speed_step 5 --stage2_t_step 0.5 --stage2_fuse_step 0.5 ^
-  --stage2_batch_size 2000 --stage2_max_cands 8000
+@REM python Q3Solver.py ^
+@REM   --dt 0.005 --nphi 360 --nz 7 ^
+@REM   --outer_backend process ^
+@REM   --backend thread --workers 1 --chunk 2200 --block 4096 ^
+@REM   --heading_min_deg -2 --heading_max_deg 2 --heading_step_deg 0.5 ^
+@REM   --speed_min 120 --speed_max 140 --speed_step 10 ^
+@REM   --t1_min 0 --t1_max 6 --t1_step 1 ^
+@REM   --gap12_min 1 --gap12_max 4 --gap12_step 1 ^
+@REM   --gap23_min 1 --gap23_max 4 --gap23_step 1 ^
+@REM   --fuse_min 3 --fuse_max 8 --fuse_step 1 ^
+@REM   --stage2_enable --stage2_span_steps 1 ^
+@REM   --stage2_heading_step_deg 0.2 --stage2_speed_step 5 --stage2_t_step 0.5 --stage2_fuse_step 0.5 ^
+@REM   --stage2_batch_size 2000 --stage2_max_cands 8000
+
+python Q3Solver_v3.py --heading_min_deg -0.5 --heading_max_deg 0.5 --heading_step_deg 0.2 --speed_min 130 --speed_max 140 --speed_step 5 --t1_min 0 --t1_max 2 --t1_step 1 --gap12_min 3 --gap12_max 5 --gap12_step 1 --gap23_min 3 --gap23_max 5 --gap23_step 1 --fuse_min 3 --fuse_max 8 --fuse_step 1 --no_cd --workers 28
